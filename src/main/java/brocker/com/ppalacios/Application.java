d407ef1f659b332d1587b54ee8eff645486350b9
Pedro Palacios Rodriguez <ppalacios1>
2021-10-17 16:52:10 +0000

    Initial commit

---
+++ src/main/java/brocker/com/ppalacios/Application.java
@@ -0,0 +1,10 @@
+package brocker.com.ppalacios;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
